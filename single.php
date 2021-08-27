6e1abb9ef1a709bdab94d9df8b275e9f72376027
Prashant Subedi <66554757+Prashant9843919843@users.noreply.github.com>
2021-08-27 17:58:47 +0545

    Update single.php

--- single.php
+++ single.php
@@ -177,7 +177,7 @@
 
 
   <!-- JQuery -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="js/jquery.min.js"></script>
 
   <!-- Slick Carousel -->
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
